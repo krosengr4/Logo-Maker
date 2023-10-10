342ebb1ebff5ba684964bf67d99dd74f2d2fbff1
krosengr4 <rosenkev4@gmail.com>
2023-10-10 03:39:19 -0500

    Added questions to ask the user

---
+++ Develope/index.js
@@ -0,0 +1,9 @@
+
+
+
+const questionsArr =[
+'Please provide 3 letters for the text of your logo:',
+'Please provide a color for the text:',
+'Please provide a shape for your logo:',
+'Please provide a color for the shape:', 
+]
