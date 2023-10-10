fe05580101e3988cfa575dd255010f99dc616a2e
krosengr4 <rosenkev4@gmail.com>
2023-10-10 03:50:39 -0500

    Added comments/ starter code

---
+++ Develope/generateLogo.js
@@ -0,0 +1,15 @@
+// Create function to return the shape of the Logo.
+
+// Create function to return the text of the Logo.
+
+// Create function to return the Color of the Shape.
+
+// Create a function to return the color of the text. 
+
+
+// Create a function to generate the file with the logo. 
+const generateLogo = () => {
+
+}
+
+module.exports = generateLogo;
--- Develope/index.js
+++ Develope/index.js
@@ -1,9 +1,25 @@
+// Include packages needed for this application
 
-
-
+// Array of questions to ask the user
 const questionsArr =[
 'Please provide 3 letters for the text of your logo:',
 'Please provide a color for the text:',
 'Please provide a shape for your logo:',
 'Please provide a color for the shape:', 
 ]
+
+// Create a prompt script
+const prompt = [
+    { 
+        type: 'input',
+        name: 'title',
+        message: questionsArr[0],
+      },
+]
+
+// Create function to initialize the prompt script and save as an SVG file called logo.svg.
+function init() {
+
+}
+
+init(); 
