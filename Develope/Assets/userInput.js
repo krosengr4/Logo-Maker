a388ca80c74089b41e2f43ae0feaa4ae01a9c223
krosengr4 <rosenkev4@gmail.com>
2023-10-25 14:50:02 -0500

    Added </svg> to end of data, moved questions array

--- Develope/Assets/userInput.js
+++ Develope/Assets/userInput.js
@@ -1,15 +1,40 @@
-//! This is the js file to get the user input. 
+//! This is the js file to get the user input. This includes the array of questions and inquirer.prompt method. 
 
 const inquirer = require('inquirer'); 
-const questions = require('./questions');
+
+const shapes = ['Square', 'Circle', 'Triangle'];
+
+// Questions to ask the user.
+const questions= [
+  { 
+    type: 'input',
+    name: 'text',
+    message: 'Please Provide Text for your Logo (up to 3 letters):',
+    validate: function(text) {
+      return text.length <= 3 && text.length > 0; //<--- Validates that no more than 3 letters go in
+    },
+  },
+  { 
+    type: 'input',   
+    name: 'textColor',
+    message: 'Please Provide a Color for the Text:',
+  },
+  { 
+    type: 'list',
+    name: 'shape',
+    choices: shapes,
+    message: 'Please Provide a Shape for your Logo:',
+  },
+  { 
+    type: 'input',
+    name: 'shapeColor',
+    message: 'Please Provide a Color for the Shape:',
+  },
+];
 
 // Function to get user input and write that user input to a svg file. 
 function userInput() {
     return inquirer.prompt(questions)
   };
-
   //// console.log(userInput());
-
 module.exports = userInput;
-
-
